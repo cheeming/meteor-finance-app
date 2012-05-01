9c2a7d9bb5c198e0f86d8c2a6f02f1a67bc7b564
Chew Chee Ming <cheeming@gmail.com>
2012-05-01 21:46:13 +0800

    Add error handling for invalid expense data
     * Rename expense.name to expense.desc
     * Minor style fixes

--- finance.js
+++ finance.js
@@ -17,6 +17,10 @@ if (Meteor.is_client) {
     Template.expenses.expenses = function () {
         return Expenses.find({}, {sort: {timestamp: -1}});
     };
+    Template.expenses.got_expenses = function () {
+        return Template.expenses.expenses().count() > 0;
+    }
+
     Template.expense.timestamp_display = function () {
         if (this.timestamp) {
             return moment(new Date(localize_timestamp_utc(this.timestamp))).format("DD/MM/YYYY");
@@ -24,19 +28,44 @@ if (Meteor.is_client) {
             return "-";
         }
     };
+    Template.expense.amount_display = function () {
+        if (this.amount) {
+            return this.amount.toFixed(2);
+        }
+        return "";
+    };
 
     Template.addexpense.events = {
         'click input.add': function () {
-            name = $(".addexpense input.name").val();
-            amount = parseFloat($(".addexpense input.amount").val());
+            Session.set("addexpense_error", "");
+            desc_str = $(".addexpense input.desc").val();
+            amount_str = $(".addexpense input.amount").val();
+            if (desc_str.length <= 0) {
+                Session.set("addexpense_error", "Please enter a description.");
+                return;
+            }
+            if (amount_str.length <= 0) {
+                Session.set("addexpense_error", "Please enter an amount.");
+                return;
+            }
+            amount = parseFloat(amount_str);
+            if (isNaN(amount)) {
+                Session.set("addexpense_error", "Please enter a valid amount.");
+                return;
+            }
             timestamp = get_timestamp_now_utc();
             Expenses.insert({
                 timestamp: timestamp,
-                name: name,
+                desc: desc_str,
                 amount: amount
             });
             $(".addexpense input[type=text]").val("");
+            $(".addexpense input.desc").focus();
         }
+    };
+
+    Template.addexpense.error  = function () {
+        return Session.get("addexpense_error");
     };
 }
 
